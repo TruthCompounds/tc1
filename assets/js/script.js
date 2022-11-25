ab45c579c932bac0f3eca49de469331c2679b8a9
eyescr3am <hanspeterbuhr@gmail.com>
2022-11-25 09:09:32 +0400

    stylize

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.height = scrolled + "%";
+
+	//scrollTop - how many pixels from top have I already scrolled
+	//scrollHeight - how many pixels on the page from top to bottom 
+	//clientHeight - height of my screen in pixels
+	
+	//how many of my screens fit on this page
+	var pageCount = document.documentElement.scrollHeight / document.documentElement.clientHeight;
+	//console.log(pageCount);
+	
+  //document.getElementById("myBarBackground").style.height = scrolled + "%";
+
+
+}
+
+function collapse(index) {
+	var postContentDiv = document.getElementById(index);
+	var disp = postContentDiv.style.display;
+	if (disp === '') 
+	{
+		postContentDiv.style.display = 'none';
+	} 
+	else if (disp === 'none') 
+	{
+		postContentDiv.style.display = '';
+	}
+	
+
+
+}
